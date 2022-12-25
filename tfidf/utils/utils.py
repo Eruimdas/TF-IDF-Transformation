d6c27e29754123786d64b1b5016446df7432bf9e
eruimdas <hasan.kemikk@gmail.com>
2022-12-25 16:16:17 +0300

    Updated: Renamed variables for ease of understanding.

--- tfidf/utils/utils.py
+++ tfidf/utils/utils.py
@@ -5,7 +5,7 @@ import numpy as np
 from scipy.sparse.dia import dia_matrix
 
 
-def partial_fit(self, X: List[str]):
+def partial_fit(self, input_data: List[str]):
     """Update the vocabulary and idf of the TfidfVectorizer.
 
     This method updates the vocabulary_ and idf_ attributes of the TfidfVectorizer with the
@@ -18,21 +18,21 @@ def partial_fit(self, X: List[str]):
     """
 
     max_idx = max(self.vocabulary_.values())
-    for a in X:
+    for data in input_data:
         # update vocabulary_
         if self.lowercase:
-            a = a.lower()
-        tokens = re.findall(self.token_pattern, a)
-        for w in tokens:
-            if w not in self.vocabulary_:
+            data = data.lower()
+        tokens = re.findall(self.token_pattern, data)
+        for token in tokens:
+            if token not in self.vocabulary_:
                 max_idx += 1
-                self.vocabulary_[w] = max_idx
+                self.vocabulary_[token] = max_idx
 
         # update idf_
         df = (self.n_docs + self.smooth_idf) / np.exp(self.idf_ - 1) - self.smooth_idf
         self.n_docs += 1
         df.resize(len(self.vocabulary_))
-        for w in tokens:
-            df[self.vocabulary_[w]] += 1
+        for token in tokens:
+            df[self.vocabulary_[token]] += 1
         idf = np.log((self.n_docs + self.smooth_idf) / (df + self.smooth_idf)) + 1
         self._tfidf._idf_diag = dia_matrix((idf, 0), shape=(len(idf), len(idf)))

a7c0d789aa45bce5cd489385a905bfcbe26a1674
eruimdas <hasan.kemikk@gmail.com>
2022-12-25 15:39:11 +0300

    Updated: Custom TF-IDF class.

---
+++ tfidf/tfidf.py
@@ -0,0 +1,212 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+from .utils.utils import partial_fit
+import pickle
+import re
+from typing import Union, List
+
+
+class CustomTfidfVectorizer:
+    """A custom TfidfVectorizer class that can fit and transform new data incrementally.
+
+    This class wraps the TfidfVectorizer class from scikit-learn, and adds the ability
+    to fit and transform new data incrementally using the partial_fit method.
+
+    Parameters
+    ----------
+    input_data : list or str, optional
+        A list of strings or a single string to be used for fitting the TfidfVectorizer.
+        Either input_data or path must be provided.
+    path : str, optional
+        The path to a pickled TfidfVectorizer object. Either input_data or path must be provided.
+    **kwargs : dict
+        Additional keyword arguments to pass to the TfidfVectorizer constructor.
+
+    Attributes
+    ----------
+    input_data : list or str
+        The input data used for fitting the TfidfVectorizer.
+    tfidf_vectorizer : TfidfVectorizer
+        The TfidfVectorizer object used for fitting and transforming data.
+    """
+
+    def __init__(
+        self,
+        input_data: Union[List[str], str, None] = None,
+        path: Union[str, None] = None,
+        **kwargs
+    ):
+        """Constructor for the CustomTfidfVectorizer class.
+
+        Either input_data or path must be provided. If input_data is provided, a new
+        TfidfVectorizer object will be created and fit on the input data. If path is provided,
+        the TfidfVectorizer object will be loaded from the specified path.
+
+        Parameters
+        ----------
+        input_data : list or str, optional
+            A list of strings or a single string to be used for fitting the TfidfVectorizer.
+            Either input_data or path must be provided.
+        path : str, optional
+            The path to a pickled TfidfVectorizer object. Either input_data or path must be provided.
+        **kwargs : dict
+            Additional keyword arguments to pass to the TfidfVectorizer constructor.
+
+        Raises
+        ------
+        ValueError
+            If neither input_data nor path is provided.
+        """
+        TfidfVectorizer.partial_fit = partial_fit
+        self.tfidf_vectorizer = TfidfVectorizer(**kwargs)
+        if path is None and input_data is not None:
+            self.input_data = input_data
+        elif path is not None and input_data is None:
+            self.load(path)
+        else:
+            raise ValueError("Either input_data or path must be provided")
+
+    def fit(self, input_data: Union[List[str], str, None] = None):
+        """Fit the TfidfVectorizer on the input data.
+
+        This method fits the TfidfVectorizer on the input data, updating the vocabulary_
+        and idf_ attributes of the TfidfVectorizer object. If input_data is not provided,
+        the input_data attribute of the CustomTfidfVectorizer object will be used.
+
+        Parameters
+        ----------
+        input_data : list or str, optional
+            A list of strings or a single string to be used for fitting the TfidfVectorizer.
+            If not provided, the input_data attribute of the CustomTfidfVectorizer object will be used.
+
+        Raises
+        ------
+        ValueError
+            If input_data is not provided and the input_data attribute of the CustomTfidfVectorizer object is not set.
+        """
+
+        if input_data is None and self.input_data is None:
+            raise ValueError("input_data must be provided")
+        elif input_data is None and self.input_data is not None:
+            input_data = self.input_data
+        self.tfidf_vectorizer = self.tfidf_vectorizer.fit(input_data)
+        self.tfidf_vectorizer.n_docs = len(input_data)
+
+    def check_new_corpus(self, new_data: Union[List[str], str]):
+        """Check for new words in the input data and fit the TfidfVectorizer on them incrementally.
+
+        This method checks for new words in the input data and fits the TfidfVectorizer on them
+        incrementally, updating the vocabulary_ and idf_ attributes of the TfidfVectorizer object.
+
+        Parameters
+        ----------
+        new_data : list or str
+            A list of strings or a single string to be checked for new words.
+        """
+
+        new_corpus = set()
+
+        if isinstance(new_data, str):
+            new_data = [new_data]
+
+        if not isinstance(new_data, list):
+            raise ValueError("new_data must be a list of strings or a single string")
+
+        # If any of the words in the input data are not in the vocabulary, add them to the new_corpus set
+        for data in new_data:
+            if any(
+                word.lower() not in self.tfidf_vectorizer.vocabulary_
+                for word in re.findall(self.tfidf_vectorizer.token_pattern, data)
+            ):
+                new_corpus.add(data)
+
+        new_corpus = list(new_corpus)
+        if len(new_corpus) > 0:
+            self.tfidf_vectorizer.partial_fit(new_corpus)
+
+    def transform(self, input_data: Union[List[str], str, None] = None):
+        """Transform the input data using the TfidfVectorizer.
+
+        This method transforms the input data using the TfidfVectorizer, checking for new words
+        in the input data and fitting the TfidfVectorizer on them incrementally if necessary.
+
+        Parameters
+        ----------
+        input_data : list or str, optional
+            A list of strings or a single string to be transformed. If not provided, the input_data
+            attribute of the CustomTfidfVectorizer object will be used.
+
+        Returns
+        -------
+        scipy.sparse.csr.csr_matrix
+            The transformed input data as a sparse matrix.
+
+        Raises
+        ------
+        ValueError
+            If input_data is not provided and the input_data attribute of the CustomTfidfVectorizer object is not set.
+        """
+
+        if input_data is None and self.input_data is None:
+            raise ValueError("input_data must be provided")
+        elif input_data is None and self.input_data is not None:
+            input_data = self.input_data
+        self.check_new_corpus(input_data)
+        transformed_data = self.tfidf_vectorizer.transform(input_data)
+        return transformed_data
+
+    def fit_transform(self, input_data: Union[List[str], str, None] = None):
+        """Fit and transform the input data using the TfidfVectorizer.
+
+        This method fits and transforms the input data using the TfidfVectorizer, updating the
+        vocabulary_ and idf_ attributes of the TfidfVectorizer object. If input_data is not provided,
+        the input_data attribute of the CustomTfidfVectorizer object will be used.
+
+        Parameters
+        ----------
+        input_data : list or str, optional
+            A list of strings or a single string to be transformed. If not provided, the input_data
+            attribute of the CustomTfidfVectorizer object will be used.
+
+        Returns
+        -------
+        scipy.sparse.csr.csr_matrix
+            The transformed input data as a sparse matrix.
+
+        Raises
+        ------
+        ValueError
+            If input_data is not provided and the input_data attribute of the CustomTfidfVectorizer object is not set.
+        """
+
+        if input_data is None:
+            input_data = self.input_data
+        transformed_data = self.tfidf_vectorizer.fit_transform(input_data)
+        return transformed_data
+
+    def save(self, file_name: str):
+        """Save the TfidfVectorizer object to a pickle file.
+
+        This method saves the TfidfVectorizer object to a pickle file with the specified file name.
+
+        Parameters
+        ----------
+        file_name : str
+            The file name to use for the pickle file.
+        """
+
+        with open(file_name, "wb") as file:
+            pickle.dump(self.tfidf_vectorizer, file)
+
+    def load(self, file_name: str):
+        """Load the TfidfVectorizer object from a pickle file.
+
+        This method loads the TfidfVectorizer object from a pickle file with the specified file name.
+
+        Parameters
+        ----------
+        file_name : str
+            The file name of the pickle file to load.
+        """
+
+        with open(file_name, "rb") as file:
+            self.tfidf_vectorizer = pickle.load(file)
---
+++ tfidf/utils/utils.py
@@ -0,0 +1,37 @@
+from scipy.sparse.dia import dia_matrix
+import numpy as np
+import re
+from typing import List
+
+
+def partial_fit(self, X: List[str]):
+    """Update the vocabulary and idf of the TfidfVectorizer.
+
+    This method updates the vocabulary_ and idf_ attributes of the TfidfVectorizer with the
+    provided list of strings.
+
+    Parameters
+    ----------
+    X : list of str
+        A list of strings to update the vocabulary_ and idf_ attributes with.
+    """
+
+    max_idx = max(self.vocabulary_.values())
+    for a in X:
+        # update vocabulary_
+        if self.lowercase:
+            a = a.lower()
+        tokens = re.findall(self.token_pattern, a)
+        for w in tokens:
+            if w not in self.vocabulary_:
+                max_idx += 1
+                self.vocabulary_[w] = max_idx
+
+        # update idf_
+        df = (self.n_docs + self.smooth_idf) / np.exp(self.idf_ - 1) - self.smooth_idf
+        self.n_docs += 1
+        df.resize(len(self.vocabulary_))
+        for w in tokens:
+            df[self.vocabulary_[w]] += 1
+        idf = np.log((self.n_docs + self.smooth_idf) / (df + self.smooth_idf)) + 1
+        self._tfidf._idf_diag = dia_matrix((idf, 0), shape=(len(idf), len(idf)))
